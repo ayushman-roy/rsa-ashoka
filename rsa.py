c727ca995919d9e2a1734889c5aff2f5f6f314b4
Ayushman Roy <AyushmanRoy09@gmail.com>
2022-04-29 20:23:37 +0530

    big update

--- rsa.py
+++ rsa.py
@@ -1,21 +1,22 @@
 # Algorithm:
+
 # Create random big number generator
 # Check for primality by LowPrime and MillerRabinTest
 # If prime make n = p * q
 # Create m = (p - 1) * (q - 1)
 # Compute e, coprime of m and very small odd integer
-# Compute d, multiplicative inverse of e modulo m 			(LEFT)
+# Compute d, multiplicative inverse of e modulo m 					(LEFT)
 # P(e,n) public key
 # S(d,n) private key
-# ASCII to message and message to ASCII converter			
+# Create M, the message converted to ASCII 							
 # P(M) = M^e mod n = C (encryption) 
 # S(C) = C^d mod n = M (decryption)
-# Implement signatures 							(LEFT)
+# Implement signatures 												(LEFT)
 # Make presentation, proofs and compute complexities for all code 	(LEFT)
 
 import random
 
-# pre-generated primes
+# pre-generated primes for low level prime test
 first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 		     31, 37, 41, 43, 47, 53, 59, 61, 67,
 		     71, 73, 79, 83, 89, 97, 101, 103,
@@ -26,21 +27,21 @@ first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 		     263, 269, 271, 277, 281, 283, 293,
 		     307, 311, 313, 317, 331, 337, 347, 349]
 
-# generates a random nBit number
-def nBitRandom(n):
+# generates a random n bit number
+def randomGenerator(n):
 	return random.randrange(2**(n-1)+1, 2**n - 1)
 
 # numbers not divisible by low primes
-def getLowLevelPrime(n):
+def lowLevelPrimeCheck(n):
 	while True:
-		pc = nBitRandom(n)
+		p = randomGenerator(n)
 		for divisor in first_primes_list:
-			if pc % divisor == 0 and divisor**2 <= pc:
+			if p % divisor == 0 and divisor**2 <= p:
 				break
 		else: 
-			return pc
+			return p
 
-# large level prime test
+# higher level prime test by Miller Rabin Test
 def isMillerRabinPassed(mrc):
 	maxDivisionsByTwo = 0
 	ec = mrc-1
@@ -65,8 +66,8 @@ def isMillerRabinPassed(mrc):
 # generates large primes
 while True:
     n = 100
-    primeCandidateA = getLowLevelPrime(n)
-    primeCandidateB = getLowLevelPrime(n)
+    primeCandidateA = lowLevelPrimeCheck(n)
+    primeCandidateB = lowLevelPrimeCheck(n)
     if not isMillerRabinPassed(primeCandidateA) and not isMillerRabinPassed(primeCandidateB):
         continue
     else: 
@@ -77,6 +78,9 @@ primeProduct = primeCandidateA * primeCandidateB
 
 # assert: m = (p - 1) * (q - 1)
 primeNewProduct = (primeCandidateA-1) * (primeCandidateB-1)
+
+print("Generated Primes: ", primeCandidateA, primeCandidateB)
+print("N = p * q and M = (p-1) * (q-1): ", primeProduct, primeNewProduct)
 
 def gcd(p,q):
     while q != 0:
@@ -93,61 +97,125 @@ while True:
 	if isCoPrime(primeNewProduct, e):
 		break
 
-# compute d: multiplicative inverse of e modulo m
+'''
 def multiplicative_inverse(e, primeNewProduct):
     d = 0
     x1 = 0
     x2 = 1
     y1 = 1
     temp_p = primeNewProduct
-
     while e > 0:
         temp1 = temp_p//e
         temp2 = temp_p - temp1 * e
         temp_p = e
         e = temp2
-
         x = x2 - temp1 * x1
         y = d - temp1 * y1
-
         x2 = x1
         x1 = x
         d = y1
         y1 = y
-
     if temp_p == 1:
-        return d + primeNewProduct
+        return d
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = egcd(b%a,a)
+    return (g, x - (b//a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('No modular inverse')
+    return x%m
+'''
+
+def modInverse(a, m):
+	m0 = m
+	y = 0
+	x = 1
+	if (m == 1):
+		return 0
+	while (a > 1):
+		# q is quotient
+		q = a // m
+		t = m
+		# m is remainder now, process
+		# same as Euclid's algo
+		m = a % m
+		a = t
+		t = y
+		# Update x and y
+		y = x - q * y
+		x = t
+	# Make x positive
+	if (x < 0):
+		x = x + m0
+	return x
+
+d = modInverse(e, primeNewProduct)
 
 # keys
 publicKey = (e, primeProduct)
 privateKey = (d, primeProduct)
 
-# converts message to ASCII
+print("Keys: ", publicKey, privateKey)
+
+# converts message to ASCII as a list
 def messageToASCIIConversion(message):
 	M = []
 	for i in message:
 		M.append(ord(i))
 	return M
 
-# input message and conversion to ASCII
+# user input: message
 message = input("Enter Your Message Here:")
-ASCIIconvertedMessage = messageConversion(message)
+
+# conversion to ASCII
+convertedMessage = messageToASCIIConversion(message)
+print("Message in ASCII:", convertedMessage)
 
 # encryption: P(M) = M^e mod n = C
 def encryption(M):
-	ciphertext = (M**e) % primeProduct
-	return ciphertext
+	encryptedList = M
+	index = len(M) - 1
+	while(index >= 0):
+		message = M[index]
+		ciphertext = (message**e) % primeProduct
+		encryptedList[index] = ciphertext
+		if (index >= 1):
+			encryptedList[index-1] += ciphertext
+		index -= 1
+	return encryptedList
+
 encryptedMessage = encryption(convertedMessage)
+print("Encrypted Message:", encryptedMessage)
+
 
 # decryption: S(C) = C^d mod n = M
 def decryption(C):
-	message = (C**d) % primeProduct
-	return message
-decryptedMessage = decryption(encryptedMessage)
+	decryptedList = C
+	temp = 0
+	index = len(C) - 1
+	while(index >= 0):
+		ciphertext = C[index]
+		message = (ciphertext**d) % primeProduct
+		decryptedList[index] = message - temp
+		if (index >= 1):
+			temp = message
+		index -= 1
+	return decryptedList
 
-# converts ASCII to message
-def ASCIIToMessageConversion(message)
+decryptedMessage = decryption(encryptedMessage)
+print("Decrypted Message: ", decryptedMessage)
+
+#converts ASCII list to message string 
+def ASCIIToMessageConversion(message):
 	R = ""
 	for val in message:
-    		R += chr(message)
+		R += chr(message)
 	return R
+
+# returns the decrypted message as string
+ASCIIToMessageConversion(decryptedMessage)
