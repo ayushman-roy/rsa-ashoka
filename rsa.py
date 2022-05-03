b8c347633bad94f31e5f04eeb9b5fb69cba1e369
Ayushman Roy <AyushmanRoy09@gmail.com>
2022-05-04 01:25:26 +0530

    fixed prime checker

--- rsa.py
+++ rsa.py
@@ -1,9 +1,58 @@
-from Crypto.Util import number
-from random import randrange
+import random
 
-# generates prime numbers of 1024 bits
-primeCandidateA = number.getPrime(1024)
-primeCandidateB = number.getPrime(1024)
+# pre-generated primes for low level prime test
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+		     107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+		     227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347]
+
+# generates a random n bit number
+def randomGenerator(n):
+	return random.randrange(2**(n-1)+1, 2**n - 1)
+
+# numbers not divisible by low primes
+def lowLevelPrimeCheck(n, list):
+	while True:
+		p = randomGenerator(n)
+		for divisor in list:
+			if p % divisor == 0 and divisor**2 <= p: break
+		else: return p
+
+# higher level prime test by Miller Rabin Test
+def isMillerRabinPassed(n):
+	maxDivisionsByTwo = 0
+	ec = n-1
+	while ec % 2 == 0:
+		ec >>= 1
+		maxDivisionsByTwo += 1
+	assert(2**maxDivisionsByTwo * ec == n-1)
+	def trialComposite(round_tester):
+		if pow(round_tester, ec, n) == 1:
+			return False
+		for i in range(maxDivisionsByTwo):
+			if pow(round_tester, 2**i * ec, n) == n-1:
+				return False
+		return True
+	numberOfRabinTrials = 230
+	for i in range(numberOfRabinTrials):
+		round_tester = random.randrange(2, n)
+		if trialComposite(round_tester):
+			return False
+	return True
+
+# generates large primes
+while True:
+	n = 330
+	# low level prime testing
+	primeCandidateA = lowLevelPrimeCheck(n, first_primes_list)
+	primeCandidateB = lowLevelPrimeCheck(n, first_primes_list)
+	# miller rabin test
+	if isMillerRabinPassed(primeCandidateA) and isMillerRabinPassed(primeCandidateB):
+		# each prime (except 2 and 3) is next to a multiple of 6 (https://bit.ly/3vYlVgc)
+		if (primeCandidateA-1)%6 == 0 or  (primeCandidateA-1)%6 == 0:
+			break
+		if (primeCandidateB+1)%6 == 0 or  (primeCandidateB+1)%6 == 0:
+			break
+	else: continue
 
 # assert: n = p * q
 primeProduct = primeCandidateA * primeCandidateB
@@ -23,7 +72,7 @@ def isCoPrime(x, y):
 
 # checks if e, m are coprimes
 while True:
-	e = randrange(2, 25)
+	e = random.randrange(2, 25)
 	if isCoPrime(primeNewProduct, e):
 		break
 		
