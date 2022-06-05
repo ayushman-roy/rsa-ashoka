950dbe78130cc054b7bb09758cd1271c6d80823e
Ayushman Roy <AyushmanRoy09@gmail.com>
2022-06-05 23:01:54 +0530

    completed version

--- rsa.py
+++ rsa.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 # pre-generated primes for low level prime test
 first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
@@ -20,45 +21,62 @@ def lowLevelPrimeCheck(n, list):
 # higher level prime test by Miller Rabin Test
 def isMillerRabinPassed(n):
 	maxDivisionsByTwo = 0
+	# even num ec since n is odd by definition 
 	ec = n-1
+	# runs ec, as long as div by 2 possible
 	while ec % 2 == 0:
+		# bitwise right shift, basically divides by 2 but since ec is odd we use bitwise
 		ec >>= 1
+		# calculates r of ec = d*2^r where d is odd
 		maxDivisionsByTwo += 1
-	assert(2**maxDivisionsByTwo * ec == n-1)
+	# checks if number is composite
 	def trialComposite(round_tester):
+		# computes x = a^d % n, if = 1 then composite, ec has changed to d
 		if pow(round_tester, ec, n) == 1:
 			return False
+		# for each i in the range of r, if a^2^ec*i % n = n-1 then composite
+		# does the following while d doesn't become n-1
+		# increases a every iteration by design, using i
 		for i in range(maxDivisionsByTwo):
 			if pow(round_tester, 2**i * ec, n) == n-1:
 				return False
 		return True
-	numberOfRabinTrials = 230
+	numberOfRabinTrials = 20
+	# runs the miller rabin test on k trials
 	for i in range(numberOfRabinTrials):
+		# picks random n between 2, n 
 		round_tester = random.randrange(2, n)
 		if trialComposite(round_tester):
 			return False
+	# returns true if not composite yet
 	return True
+
+startTime = time.time()
 
 # generates large primes
 while True:
-	n = 330
+	n = 450
 	# low level prime testing
 	primeCandidateA = lowLevelPrimeCheck(n, first_primes_list)
 	primeCandidateB = lowLevelPrimeCheck(n, first_primes_list)
 	# miller rabin test
 	if isMillerRabinPassed(primeCandidateA) and isMillerRabinPassed(primeCandidateB):
 		# each prime (except 2 and 3) is next to a multiple of 6 (https://bit.ly/3vYlVgc)
-		if (primeCandidateA-1)%6 == 0 or  (primeCandidateA-1)%6 == 0:
-			break
-		if (primeCandidateB+1)%6 == 0 or  (primeCandidateB+1)%6 == 0:
-			break
+		if (primeCandidateA-1)%6 == 0 or  (primeCandidateA+1)%6 == 0:
+			if (primeCandidateB-1)%6 == 0 or  (primeCandidateB+1)%6 == 0:
+				break
 	else: continue
+
+print("Time Elapsed: ", time.time() - startTime)
 
 # assert: n = p * q
 primeProduct = primeCandidateA * primeCandidateB
 
 # assert: m = (p - 1) * (q - 1)
 primeNewProduct = (primeCandidateA-1) * (primeCandidateB-1)
+
+print("Generated Primes: ", primeCandidateA, primeCandidateB)
+print("N = p * q and M = (p-1) * (q-1): ", primeProduct, primeNewProduct)
 
 # calculates gcd
 def gcd(p,q):
@@ -115,14 +133,21 @@ convertedMessage = messageToASCIIConversion(message)
 # Reference: Geek for Geeks
 # calculates (x^y)%p
 def power(x, y, p) :
+	# stores the value
 	res = 1
+	# first calculates for y = 1
 	x = x % p
+	# base case
 	if (x == 0) :
 		return 0
 	while (y > 0) :
+		# bitwise check on y and 1 that they are equal to 1, fancy way of checking y = 1 
+		# in that case it is the last iter and hence stores to res
 		if ((y & 1) == 1) :
 			res = (res * x) % p
+		# divides y by 2
 		y = y >> 1	
+		# calculates new val of x^2 % p
 		x = (x * x) % p
 	return res
 
@@ -157,9 +182,7 @@ def decryption(C):
 	index = len(C) - 1
 	while(index >= 0):
 		ciphertext = decryptedList[index]
-		# decryptedList[index] -= temp
 		message = power(ciphertext, d, primeProduct)
-		# decryptedList[index] += message
 		decryptedList[index] = message - temp
 		if (index >= 1):
 			temp = ciphertext
